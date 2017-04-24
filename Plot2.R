5320614148b6d5fc11fd058695091e16fcd20d4d
cdtaggart <colin.taggart@zsassociates.com>
2017-04-24 09:40:35 -0400

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,17 @@
+# Reading, naming and subsetting
+ActivePower <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(ActivePower) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subActivePower <- subset(ActivePower,power$Date=="1/2/2007" | ActivePower$Date =="2/2/2007")
+
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subActivePower$Date <- as.Date(subActivePower$Date, format="%d/%m/%Y")
+subActivePower$Time <- strptime(subActivePower$Time, format="%H:%M:%S")
+subActivePower[1:1440,"Time"] <- format(subActivePower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subActivePower[1441:2880,"Time"] <- format(subActivePower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+
+# Basic plot function
+plot(subActivePower$Time,as.numeric(as.character(subActivePower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+
+# Annotating graph
+title(main="Global Active Power Vs Time")
---
+++ Plot4.R
@@ -0,0 +1,25 @@
+# Reading, naming and subsetting 
+ActivePower <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(ActivePower) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subActivePower <- subset(ActivePower,ActivePower$Date=="1/2/2007" | ActivePower$Date =="2/2/2007")
+
+# Transforming vars
+subActivePower$Date <- as.Date(subActivePower$Date, format="%d/%m/%Y")
+subActivePower$Time <- strptime(subActivePower$Time, format="%H:%M:%S")
+subActivePower[1:1440,"Time"] <- format(subActivePower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subActivePower[1441:2880,"Time"] <- format(subActivePower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+# Composite
+par(mfrow=c(2,2))
+
+# Basic plot function calling different functions to build each plot
+with(subActivePower,{
+  plot(subActivePower$Time,as.numeric(as.character(subActivePower$Global_active_power)),type="l",  xlab="",ylab="Global Active Power")  
+  plot(subActivePower$Time,as.numeric(as.character(subActivePower$Voltage)), type="l",xlab="datetime",ylab="Voltage")
+  plot(subActivePower$Time,subActivePower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+  with(subActivePower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+  with(subActivePower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+  with(subActivePower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+  legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
+  plot(subActivePower$Time,as.numeric(as.character(subpower$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
+})
